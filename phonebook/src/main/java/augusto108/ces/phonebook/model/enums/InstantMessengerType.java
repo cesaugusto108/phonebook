907d35f45041728e473ab94f6c250e2aa9d5b313
Cesar Augusto <ces.augusto108@gmail.com>
2023-10-14 10:15:23 -0300

    InstantMessengerType enum new values added

--- phonebook/src/main/java/augusto108/ces/phonebook/model/enums/InstantMessengerType.java
+++ phonebook/src/main/java/augusto108/ces/phonebook/model/enums/InstantMessengerType.java
@@ -2,5 +2,5 @@ package augusto108.ces.phonebook.model.enums;
 
 public enum InstantMessengerType {
 
-    AIM, DISCORD, HANGOUTS, ICQ, JABBER, OTHER, QQ, SKYPE, TELEGRAM, WINDOWS_LIVE, YAHOO
+    AIM, DISCORD, FACEBOOK, HANGOUTS, ICQ, INSTAGRAM, JABBER, OTHER, QQ, SKYPE, TELEGRAM, WINDOWS_LIVE, YAHOO
 }
