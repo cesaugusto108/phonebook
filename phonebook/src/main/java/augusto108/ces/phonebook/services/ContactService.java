17c54797bbfd804964bea42eaf6430ab468a7357
Cesar Augusto <cesaraugusto@skiff.com>
2023-10-17 10:39:07 -0300

    ContactService implementation - findAll and findById

---
+++ phonebook/src/main/java/augusto108/ces/phonebook/services/ContactService.java
@@ -0,0 +1,11 @@
+package augusto108.ces.phonebook.services;
+
+import augusto108.ces.phonebook.model.dto.ContactDto;
+import org.springframework.data.domain.Page;
+
+public interface ContactService {
+
+    Page<ContactDto> findAllContacts(int page, int size);
+
+    ContactDto findContactById(String id);
+}
---
+++ phonebook/src/main/java/augusto108/ces/phonebook/services/ContactServiceImpl.java
@@ -0,0 +1,36 @@
+package augusto108.ces.phonebook.services;
+
+import augusto108.ces.phonebook.model.dto.ContactDto;
+import augusto108.ces.phonebook.model.entities.Contact;
+import augusto108.ces.phonebook.model.mapper.DtoMapper;
+import augusto108.ces.phonebook.repository.ContactRepository;
+import jakarta.persistence.NoResultException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ContactServiceImpl implements ContactService {
+
+    private final ContactRepository contactRepository;
+
+    public ContactServiceImpl(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @Override
+    public Page<ContactDto> findAllContacts(int page, int size) {
+        final Page<Contact> contacts = contactRepository.findAll(PageRequest.of(page, size));
+        return contacts.map(DtoMapper::fromContactToContactDto);
+    }
+
+    @Override
+    public ContactDto findContactById(String id) {
+        final Contact contact = contactRepository
+                .findById(UUID.fromString(id))
+                .orElseThrow(() -> new NoResultException("No result found for id: " + id));
+        return DtoMapper.fromContactToContactDto(contact);
+    }
+}
