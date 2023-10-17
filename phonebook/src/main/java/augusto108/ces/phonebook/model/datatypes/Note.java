0956f1c89f8a5aca01313c62b5dd46b4ad246f93
Cesar Augusto <ces.augusto108@gmail.com>
2023-10-16 22:34:43 -0300

    Added @Column JPA annotation to note field in Note class

--- phonebook/src/main/java/augusto108/ces/phonebook/model/datatypes/Note.java
+++ phonebook/src/main/java/augusto108/ces/phonebook/model/datatypes/Note.java
@@ -1,9 +1,10 @@
 package augusto108.ces.phonebook.model.datatypes;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
-public record Note(String note) {
+public record Note(@Column(name = "note") String note) {
 
     public Note() {
         this("");
