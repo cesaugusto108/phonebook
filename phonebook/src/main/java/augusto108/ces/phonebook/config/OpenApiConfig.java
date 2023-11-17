7e39838893d51ec9b4358c996eb3f6a4625f294b
Cesar Augusto <cesaraugusto@skiff.com>
2023-11-16 23:45:57 -0300

    OpenApi Swagger configuration bean

---
+++ phonebook/src/main/java/augusto108/ces/phonebook/config/OpenApiConfig.java
@@ -0,0 +1,15 @@
+package augusto108.ces.phonebook.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPIConfig() {
+        return new OpenAPI().info(new Info().title("Phonebook").description("Phonebook application").version("v1"));
+    }
+}
