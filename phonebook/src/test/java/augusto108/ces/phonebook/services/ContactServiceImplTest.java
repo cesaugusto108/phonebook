15d9d151c61850cd794a46c1e7b2d067077c0a0f
Cesar Augusto <cesaraugusto@skiff.com>
2023-12-18 16:09:26 -0300

    ContactServiceImplTest update (updateContact)

--- phonebook/src/test/java/augusto108/ces/phonebook/services/ContactServiceImplTest.java
+++ phonebook/src/test/java/augusto108/ces/phonebook/services/ContactServiceImplTest.java
@@ -106,7 +106,7 @@ class ContactServiceImplTest extends TestContainersConfiguration {
         assertEquals("e8fd1a04-1c85-45e0-8f35-8ee8520e1800", dto.getId().toString()); // checks id is the same
 
         dto.setFirstName("Joana");
-        dto = contactService.saveContact(dto);
+        dto = contactService.updateContact("e8fd1a04-1c85-45e0-8f35-8ee8520e1800", dto);
         assertEquals("Joana", dto.getFirstName()); // checks the obj returned from the method has name set before
 
         contact = contactRepository
@@ -118,7 +118,7 @@ class ContactServiceImplTest extends TestContainersConfiguration {
         assertEquals(13, contacts.size()); // checks number of objs stays the same
 
         dto.setFirstName("Robson"); // sets objs name back to original
-        dto = contactService.saveContact(dto);
+        dto = contactService.updateContact("e8fd1a04-1c85-45e0-8f35-8ee8520e1800", dto);
         assertEquals("Robson", dto.getFirstName()); // checks the obj returned from the method has name set before
 
         contact = contactRepository
