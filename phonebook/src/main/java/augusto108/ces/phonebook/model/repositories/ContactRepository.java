50a4e43432522fb3c97d043a14ac6d79904983a3
Cesar Augusto <cesaraugusto@tutanota.de>
2024-10-01 15:49:18 -0300

    Added @Transactional annotation to ContactRepository interface

--- phonebook/src/main/java/augusto108/ces/phonebook/model/repositories/ContactRepository.java
+++ phonebook/src/main/java/augusto108/ces/phonebook/model/repositories/ContactRepository.java
@@ -7,10 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
-@Repository public interface ContactRepository extends JpaRepository<Contact, UUID>
+@Transactional(readOnly = true)
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, UUID>
 {
 
 	@Query("from Contact c where " +
