faa61cb798694c1285c296fb992f013f06bc42a9
Cesar Augusto <cesaraugusto@tutanota.de>
2024-10-03 10:16:49 -0300

    .editorconfig

--- phonebook/src/main/resources/db/migration/test/V2__Insert_Data_Address.sql
+++ phonebook/src/main/resources/db/migration/test/V2__Insert_Data_Address.sql
@@ -1,30 +1,30 @@
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (1, 'Castelo Branco', '319', 'Apt 102', NULL, '70240-098', 'São Pedro', 'Minas Gerais', 'Brasil', 'HOME');
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (2, 'Porto da Folha', '39', NULL, NULL, NULL, 'Aracaju', 'Sergipe', 'Brasil', 'HOME');
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (3, 'Ulisses Guimarães', '215', NULL, NULL, NULL, 'Rio Janeiro', 'Rio Janeiro', 'Brasil', 'WORK');
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (4, 'Ayrton Senna', '31', 'Apt 101', NULL, NULL, 'Uberlândia', 'Minas Gerais', 'Brasil', 'HOME');
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (5, 'Dep. Fernando Rocha', '2219', 'Apt 505', NULL, NULL, 'São Pedro', 'Minas Gerais', 'Brasil', 'HOME');
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (6, 'Boquim', '59', NULL, NULL, '49240-098', 'Aracaju', 'Sergipe', 'Brasil', 'HOME');
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (7, 'Castelo Branco', '449', NULL, NULL, '70240-098', 'São Pedro', 'Minas Gerais', 'Brasil', 'WORK');
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (8, 'João Figueiredo', '3122', NULL, NULL, '30240-098', 'Porto Alegre', 'Rio Grande do Sul', 'Brasil', NULL);
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (9, 'Main', '1449', NULL, NULL, NULL, 'Santa Monica', 'California', 'United States', 'OTHER');
 INSERT INTO address (id, street, number, complement, district, postal_code, city_name, country_state, country_name,
-                     address_type)
+		     address_type)
 VALUES (10, 'Sunset', '3244', NULL, NULL, NULL, 'San Diego', 'California', 'United States', 'WORK');
--- phonebook/src/main/resources/db/migration/test/V6__Insert_Data_Contact.sql
+++ phonebook/src/main/resources/db/migration/test/V6__Insert_Data_Contact.sql
@@ -1,79 +1,79 @@
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1800', 'Robson', 'Santos', 'Pereira', NULL, NULL, NULL, 'ASSISTANT', NULL,
-        NULL,
-        NULL, NULL, NULL, NULL);
+	NULL,
+	NULL, NULL, NULL, NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1801', 'Rebeca', 'Alves', 'Souza', NULL, NULL, NULL, 'PARTNER', NULL, NULL,
-        NULL,
-        NULL, NULL, NULL);
+	NULL,
+	NULL, NULL, NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1802', 'Carla', 'Prado', 'Campos', NULL, NULL, NULL, 'FRIEND', NULL, NULL,
-        NULL,
-        '1991-06-09', 'BIRTHDAY', NULL);
+	NULL,
+	'1991-06-09', 'BIRTHDAY', NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1803', 'Alberto', NULL, 'Pereira', NULL, NULL, NULL, 'RELATIVE', NULL, NULL,
-        NULL,
-        NULL, NULL, NULL);
+	NULL,
+	NULL, NULL, NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1804', 'Lucas', 'Santos', 'Silva', NULL, NULL, NULL, 'BROTHER', NULL, NULL,
-        NULL,
-        NULL, NULL, NULL);
+	NULL,
+	NULL, NULL, NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1805', 'Ana', 'Santos', 'Silva', NULL, NULL, NULL, 'SISTER', NULL, NULL, NULL,
-        NULL, NULL, NULL);
+	NULL, NULL, NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1806', 'Beatriz', 'Dantas', 'Carvalho', NULL, NULL, NULL, NULL, NULL, NULL,
-        NULL,
-        NULL, NULL, NULL);
+	NULL,
+	NULL, NULL, NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1807', 'Josefa', 'Santos', 'Pires', NULL, NULL, NULL, 'RELATIVE', NULL, NULL,
-        NULL,
-        NULL, NULL, NULL);
+	NULL,
+	NULL, NULL, NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1808', 'Kelly', 'Santos', 'Pires', NULL, NULL, NULL, 'RELATIVE', NULL, NULL,
-        NULL,
-        NULL, NULL, NULL);
+	NULL,
+	NULL, NULL, NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1809', 'Miguel', 'Santos', 'Silva', NULL, NULL, NULL, 'FATHER', NULL, NULL,
-        NULL,
-        '1961-10-19', 'BIRTHDAY', NULL);
+	NULL,
+	'1961-10-19', 'BIRTHDAY', NULL);
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1910', 'Charles', NULL, 'Smith', NULL, NULL, NULL, 'PARTNER', 'DEV4U', 'Mr',
-        'dev4u.com',
-        NULL, NULL, 'Lorem ipsum ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt');
+	'dev4u.com',
+	NULL, NULL, 'Lorem ipsum ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt');
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1911', 'Carl', 'Phillip', 'Jackson', NULL, NULL, NULL, 'PARTNER', 'DEV4U',
-        'Mr',
-        NULL, NULL, NULL,
-        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+	'Mr',
+	NULL, NULL, NULL,
+	'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
 INSERT INTO contact (id, first_name, middle_name, last_name, phonetic_first_name, phonetic_middle_name,
-                     phonetic_last_name, relationship, company, title, website, date, date_type, note)
+		     phonetic_last_name, relationship, company, title, website, date, date_type, note)
 VALUES ('e8fd1a04-1c85-45e0-8f35-8ee8520e1912', 'Ângela', 'Martins', 'Silva', NULL, NULL, NULL, 'SPOUSE', NULL, NULL,
-        NULL,
-        NULL, NULL, 'Tortor posuere ac ut consequat semper');
+	NULL,
+	NULL, NULL, 'Tortor posuere ac ut consequat semper');
 
 
